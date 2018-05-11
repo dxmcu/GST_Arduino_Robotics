3f6180c77255efcfafaf90cc76e64e276092cf8d
udave2007@gmail.com <udave2007@gmail.com>
2018-05-10 22:26:19 -0400

    Testing time delays for ping errors

--- 2018GSTWk1/Extras/NewPing_Testing/NewPing_Testing.ino
+++ 2018GSTWk1/Extras/NewPing_Testing/NewPing_Testing.ino
@@ -1,11 +1,11 @@
 /* NewPing_Testing.ino
 
-	JUST FOR TESTING the different functions in NewPing Library and to see if the
-	HC-SR04 can be made more reliable
-	sonar.ping_median
-	
-	This program is based on Servo_Sonic.ino code; including keeping the DC motor and Servo motor
-	functionality to see if they are introducing spurious noise.
+    JUST FOR TESTING the different functions in NewPing Library and to see if the
+    HC-SR04 can be made more reliable
+    sonar.ping_median
+    
+    This program is based on Servo_Sonic.ino code; including keeping the DC motor and Servo motor
+    functionality to see if they are introducing spurious noise.
 
     Arduino: Arduino Mega 256 v3 Clone
     Motor Shield: Generic clone of Adafruit Motor Shield for Arduino v2
@@ -21,17 +21,17 @@
 #include <Servo.h>
 
 #if defined PING_MEDIAN_DELAY
-	#undef PING_MEDIAN_DELAY
-	#define PING_MEDIAN_DELAY 2000
+    #undef PING_MEDIAN_DELAY
+    #define PING_MEDIAN_DELAY 29000
 #else
-	#define PING_MEDIAN_DELAY 2000
+    #define PING_MEDIAN_DELAY 29000
 #endif
-	
+    
 #if defined NO_ECHO
-	#undef NO_ECHO
-	#define NO_ECHO 65535               // Value returned if there's no ping echo within the specified MAX_SENSOR_DISTANCE or max_cm_distance. Default=0
+    #undef NO_ECHO
+    #define NO_ECHO 65535               // Value returned if there's no ping echo within the specified MAX_SENSOR_DISTANCE or max_cm_distance. Default=0
 #else
-	#define NO_ECHO 65535
+    #define NO_ECHO 65535
 #endif
 
 // Create the motor shield object with the default I2C address
@@ -77,37 +77,38 @@ const byte TILTSERVOPIN = 9; // Servo 2
 float pingDist; // define variable to use for distance measurements
 String msg="";
 unsigned long thisMean;
+unsigned int pingTime;
 
 /* This code is based on the NewPing::ping_median function found in the 
-	NewPing library.
-	
-	Changed from a median calculation to the mean over it non-NO_ECHO returns.
-	Note that NO_ECHO is defined in NewPing.h
-	*/
+    NewPing library.
+    
+    Changed from a median calculation to the mean over it non-NO_ECHO returns.
+    Note that NO_ECHO is defined in NewPing.h
+    */
 unsigned long ping_mean(uint8_t it=5, unsigned int max_cm_distance=500) {
-	unsigned int last;
-	uint8_t  i = 0;
-	unsigned long sum_pings, t;
+    unsigned int last;
+    uint8_t  i = 0;
+    unsigned long sum_pings, t;
 
-	while (i < it) {
-		t = micros();                  // Start ping timestamp.
-		last = sonic.ping(max_cm_distance);  // Send ping.
+    while (i < it) {
+        t = micros();                  // Start ping timestamp.
+        last = sonic.ping(max_cm_distance);  // Send ping.
 
-		if (last != NO_ECHO && last != 65535) {         // Ping in range, include as part of sum.
-			if (i > 0) {               // Don't start sort till second ping.
-				sum_pings = sum_pings + last;     // Add latest ping to sum
-			} else sum_pings = last;   // Initialize sum_pings with first ping.
-			i++;                       // Move to next ping.
-		}
-		// else it--;                 // Ping out of range, skip and don't include as part of sum.
-		// Serial.println(String("i, ping, sum_pings = " + String(i) + ", " + String(last) + ", " + String(sum_pings)));
+        if (last != NO_ECHO && last > 0) {         // Ping in range, include as part of sum.
+            if (i > 0) {               // Don't start sort till second ping.
+                sum_pings = sum_pings + last;     // Add latest ping to sum
+            } else sum_pings = last;   // Initialize sum_pings with first ping.
+            i++;                       // Move to next ping.
+        }
+        // else it--;                 // Ping out of range, skip and don't include as part of sum.
+        // Serial.println(String("i, ping, sum_pings = " + String(i) + ", " + String(last) + ", " + String(sum_pings)));
 
-		if (i < it && micros() - t < PING_MEDIAN_DELAY) {
-			// Serial.println("Delay in ping_mean");
-			delay((PING_MEDIAN_DELAY + t - micros()) / 1000); // Millisecond delay between pings.
-		}
-	}
-	return (sum_pings/it); // Return the ping distance mean.
+        if (i < it && micros() - t < PING_MEDIAN_DELAY) {
+            // Serial.println("Delay in ping_mean");
+            delay((PING_MEDIAN_DELAY + t - micros()) / 1000); // Millisecond delay between pings.
+        }
+    }
+    return (sum_pings/it); // Return the ping distance mean.
 }
 
   
@@ -138,52 +139,59 @@ void setup(void){
 void loop(){
 
   // Test some of the sonic library functions:  
-/* 	  Serial.print(sonic.ping_in());
-	  Serial.print(" inches, cm = ");
-	  Serial.print(sonic.ping_cm());
-	  Serial.print(", ping time (ms) = ");
-	  // int ping_milli = sonic.ping();
-	  int ping_milli = sonic.ping_median();
-	  Serial.print(ping_milli);
-	  Serial.print(", real inches = ");
-	  Serial.print(Distance_inches(ping_milli));
-	  Serial.print(", real cm = ");
-	  Serial.print(Distance_cm(ping_milli));
-	  Serial.print(", med ping = ");
-	  Serial.println(sonic.ping_median(7,500));
- */	  // delay(100); // Just to slow things down
+/*    Serial.print(sonic.ping_in());
+      Serial.print(" inches, cm = ");
+      Serial.print(sonic.ping_cm());
+      Serial.print(", ping time (ms) = ");
+      // int ping_milli = sonic.ping();
+      int ping_milli = sonic.ping_median();
+      Serial.print(ping_milli);
+      Serial.print(", real inches = ");
+      Serial.print(Distance_inches(ping_milli));
+      Serial.print(", real cm = ");
+      Serial.print(Distance_cm(ping_milli));
+      Serial.print(", med ping = ");
+      Serial.println(sonic.ping_median(7,500));
+ */   // delay(100); // Just to slow things down
  
  
-	msg = "\r\n10 lines with sonic.ping()\r\n";
-	for (int i=1; i<=10; i++) {
-		for (int j=1; j<10; j++) {
-			msg += String(String(sonic.ping()) + "\t");
-		}
-		Serial.println(String(msg + String(sonic.ping())));
-		msg = "";
-	}
+    msg = "\r\n10 lines with sonic.ping()\r\n";
+    for (int i=1; i<=10; i++) {
+        for (int j=1; j<10; j++) {
+            pingTime = sonic.ping(200);  // Send ping.
+            if (pingTime != NO_ECHO && pingTime > 0) {         // Ping in range, include as part of sum.
+                msg += String(String(pingTime) + "\t");
+			}
+			else {
+				j--;
+			}
+            delay(PING_MEDIAN_DELAY/1000);
+        }
+        Serial.println(String(msg + String(sonic.ping())));
+        msg = "";
+    }
 
-	msg = "\r\n10 lines with sonic.ping_median()\r\n";
-	for (int i=1; i<=10; i++) {
-		for (int j=1; j<10; j++) {
-			msg += String(String(sonic.ping_median(5,200)) + "\t");
-		}
-		Serial.println(String(msg + String(sonic.ping_median(5,200))));
-		msg = "";
-	}
+    msg = "\r\n10 lines with sonic.ping_median()\r\n";
+    for (int i=1; i<=10; i++) {
+        for (int j=1; j<10; j++) {
+            msg += String(String(sonic.ping_median(5,200)) + "\t");
+        }
+        Serial.println(String(msg + String(sonic.ping_median(5,200))));
+        msg = "";
+    }
 
-	msg = "\r\n10 lines with ping_mean()\r\n";
-	for (int i=1; i<=10; i++) {
-		for (int j=1; j<10; j++) {
-			msg += String(String(ping_mean(5,200)) + "\t");
-		}
-		Serial.println(String(msg + String(ping_mean(5,200))));
-		msg = "";
-	}
+    msg = "\r\n10 lines with ping_mean()\r\n";
+    for (int i=1; i<=10; i++) {
+        for (int j=1; j<10; j++) {
+            msg += String(String(ping_mean(5,200)) + "\t");
+        }
+        Serial.println(String(msg + String(ping_mean(5,200))));
+        msg = "";
+    }
 
-	// for (int i = 1; i <=5; i++) {
-		// thisMean = ping_mean(3,500);
-		// Serial.println(String("Ping Mean is " + String(thisMean) + ", Dist is " + String(thisMean/ US_ROUNDTRIP_IN)));
-	// }
-	while(digitalRead(RIGHT_BUMP_PIN)) {}  //Stop after going through the loop once and wait for right bump switch	
+    // for (int i = 1; i <=5; i++) {
+        // thisMean = ping_mean(3,500);
+        // Serial.println(String("Ping Mean is " + String(thisMean) + ", Dist is " + String(thisMean/ US_ROUNDTRIP_IN)));
+    // }
+    while(digitalRead(RIGHT_BUMP_PIN)) {}  //Stop after going through the loop once and wait for right bump switch  
 }
